c9a0b3a08e8f9a8ce4ace2cd899ae29019eac540
“JunHao <2414816h@student.gla.ac.uk>
2020-01-30 01:12:27 +0000

    Chapter3 Test OK

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -15,7 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+from rango import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('admin/', admin.site.urls),
+#    url(r'^$', views.index, name='index'), url(r'^rango/', include('rango.urls')),
+    path('rango/', include('rango.urls')),
+    
 ]
+
+
